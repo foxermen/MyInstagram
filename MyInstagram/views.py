cd8b58e43a91d64d96287d7798beb879b94270bd
foxermen <ardfzelenskiyfox@gmail.com>
2016-04-11 23:13:41 +0300

    Add template user_profile

--- MyInstagram/views.py
+++ MyInstagram/views.py
@@ -1,9 +1,22 @@
 # coding=utf-8
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from MyInstagram.models import User
+from django.http import Http404
 
 
 def user_main(request, username, page='1'):
-    return HttpResponse('%s %s' % (username, page))
+    try :
+        p = int(page)
+    except ValueError:
+        raise Http404
+
+    user = get_object_or_404(User, username=username)
+    posts = user.user_posts.all()[:]
+    if (p - 1) * 10 >= len(posts):
+        raise Http404
+
+    posts = posts[(p - 1) * 10:p * 10]
+    return render(request, 'user_profile.html', context={"username": username, "posts": posts})
 
 
 def user_post(request, username, post_id):
